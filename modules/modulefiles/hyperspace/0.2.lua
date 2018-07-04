c3a42fbb6f2a9a212f39b9a7edbf2d518e395f27
Todd <myoung44@vols.utk.edu>
2018-07-04 00:23:32 -0400

    Adding modulefiles

---
+++ modules/modulefiles/hyperspace/0.2.lua
@@ -0,0 +1,12 @@
+-- -*- lua -*-
+-- -- Module file created by Todd Young. April 2, 2018
+-- --
+whatis([[Name : hyperspace]])
+whatis([[Version : 0.2]])
+help([[Library fro distributed bayesian optimization]])
+
+load("python/3.6.4")
+
+prepend_path("PATH", "/gpfs/alpinetds/proj-shared/csc276/modules/hyperspace/0.2/bin")
+prepend_path("LIBRARY_PATH", "/gpfs/alpinetds/proj-shared/csc276/modules/hyperspace/0.2/lib")
+prepend_path("PYTHONPATH", "/gpfs/alpinetds/proj-shared/csc276/modules/hyperspace/0.2/lib/python3.6/site-packages")
