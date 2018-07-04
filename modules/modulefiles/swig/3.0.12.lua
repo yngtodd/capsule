27e1bdd4af8f304a9f15e1563cd96c25f6a269f1
Todd <myoung44@vols.utk.edu>
2018-07-04 00:25:04 -0400

    Adding swig.lua

---
+++ modules/modulefiles/swig/3.0.12.lua
@@ -0,0 +1,10 @@
+-- -*- lua -*-
+-- -- Module file created by Todd Young. 03 July, 2018
+-- --
+whatis([[Name : swig]])
+whatis([[Version : 3.0.12]])
+help([[]])
+
+load("python/3.6.4")
+
+prepend_path("PATH", "/gpfs/alpinetds/proj-shared/csc276/modules/swig/3.0.12/bin")
